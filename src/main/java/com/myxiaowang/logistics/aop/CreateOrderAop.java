a328e1ae5b4dcd9fede1d24961d87355031c9236
Myk <17360402335@163.com>
2022-03-26 16:43:50 +0800

    提交订单的bug

--- src/main/java/com/myxiaowang/logistics/aop/CreateOrderAop.java
+++ src/main/java/com/myxiaowang/logistics/aop/CreateOrderAop.java
@@ -1,14 +1,12 @@
 package com.myxiaowang.logistics.aop;
 
 
-import com.alibaba.fastjson.JSON;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.myxiaowang.logistics.dao.UserMapper;
 import com.myxiaowang.logistics.pojo.Order;
 import com.myxiaowang.logistics.pojo.User;
 import com.myxiaowang.logistics.util.RedisUtil.RedisPool;
 import com.myxiaowang.logistics.util.Reslut.ResponseResult;
-import org.apache.logging.log4j.util.Strings;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
@@ -17,8 +15,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import redis.clients.jedis.Jedis;
-
 
 
 /**
@@ -39,7 +35,8 @@ public class CreateOrderAop {
     private RedisPool redisPool;
 
     @Pointcut(value = "execution(* com.myxiaowang.logistics.controller.OrderController.createOrder(..))")
-    public void point(){}
+    public void point() {
+    }
 
 
     @Around(value = "point()")
@@ -47,17 +44,9 @@ public class CreateOrderAop {
         Object[] args = pjp.getArgs();
         Order arg = (Order) args[0];
         System.out.println(arg);
-        try(Jedis jedis=redisPool.getConnection()) {
-            String s = jedis.get(arg.getUserId());
-            User user;
-            if (Strings.isEmpty(s)) {
-                user = userMapper.selectOne(new LambdaQueryWrapper<User>().eq(User::getUserid, arg.getUserId()));
-            } else {
-                user = JSON.parseObject(s, User.class);
-            }
-            if (user.getDecimals().compareTo(arg.getMoney()) < 0) {
-                return ResponseResult.error("金额不足，不能生成订单");
-            }
+        User user = userMapper.selectOne(new LambdaQueryWrapper<User>().eq(User::getUserid, arg.getUserId()));
+        if (user.getDecimals().compareTo(arg.getMoney()) < 0) {
+            return ResponseResult.error("金额不足，不能生成订单");
         }
         return pjp.proceed();
     }
