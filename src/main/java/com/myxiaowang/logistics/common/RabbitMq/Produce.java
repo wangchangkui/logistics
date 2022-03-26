287b2adee3e1687b6040fca8c77d0613c5b21c4f
Myk <17360402335@163.com>
2022-03-26 16:22:43 +0800

    用户BUG

--- src/main/java/com/myxiaowang/logistics/common/RabbitMq/Produce.java
+++ src/main/java/com/myxiaowang/logistics/common/RabbitMq/Produce.java
@@ -24,42 +24,12 @@ import java.util.Objects;
  */
 @Component
 public class Produce {
-   private final Logger logger = LoggerFactory.getLogger(Produce.class);
 
     @Autowired
     private RabbitTemplate rabbitTemplate;
 
     @Autowired
     private PropertiesConfig propertiesConfig;
-
-    private final RabbitTemplate.ConfirmCallback confirmCallback = (correlationData, ack, cause) -> {
-        logger.info("correlationData : " + correlationData);
-
-        logger.info("ack : " + ack);
-        if (!ack) {
-            logger.error("ConfirmCallback发生异常....");
-        }
-    };
-
-    private final RabbitTemplate.ReturnCallback returnCallback = (message, replyCode, replyText, exchange, routingKey) -> {
-        logger.info("message : " + message);
-        logger.info("replyCode : " + replyCode);
-        logger.info("replyText : " + replyText);
-        logger.info("exchange : " + exchange);
-        logger.info("routingKey : " + routingKey);
-    };
-
-    public void sendMsg(Object message, Map<String, Object> properties) {
-        MessageHeaders messageHeaders = new MessageHeaders(properties);
-        Message<Object> msg = MessageBuilder.createMessage(message, messageHeaders);
-        // 确认回调
-        rabbitTemplate.setConfirmCallback(confirmCallback);
-        // 消息回调
-        rabbitTemplate.setReturnCallback(returnCallback);
-        // 附加消息
-        CorrelationData correlationData = new CorrelationData("myxiaowang");
-        rabbitTemplate.convertAndSend(propertiesConfig.getExchange(), propertiesConfig.getRoute_key(), msg, correlationData);
-    }
 
     public void sendMsg(String message){
         rabbitTemplate.convertAndSend(propertiesConfig.getExchange(), propertiesConfig.getRoute_key(),message);
